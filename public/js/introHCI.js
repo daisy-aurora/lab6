1eb20e0b4186194fd0360a493f75688b33857d93
Daisy Aurora <daurora@ucsdedu>
2016-02-11 18:23:28 +0000

    ajakx lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,20 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/' + idNumber, addProject);
+	console.log('/project/' + idNumber);
+}
+
+function addProject(result) {
+  console.log(result);
+  var projectHTML =
+    '<img src="' + result['image'] + '" class="detalsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p> <small>' + result['date'] + '</small></p>' +
+    '<p>' + result['summary'] + '</p>';
+
+    $("#project" + result['id'] + " .details").html(projectHTML); 
 }
 
 /*
@@ -34,5 +48,19 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	e.preventDefault();
+
+	$.get('/palette', addColor);
+}
+
+function addColor(result) {
+	console.log(result);
+
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
